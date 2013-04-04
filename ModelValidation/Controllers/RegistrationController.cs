2720d985157e7b78d7e85db6af64b57153caacda
unknown <arnab.roychowdhury@WSSYS34.webspiders.com>
2013-04-04 12:37:32 +0530

    Initial commit.

---
+++ ModelValidation/Controllers/RegistrationController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IRegistrationRepositary RegisterRepo { get; set; }
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            if (RegisterRepo == null)
+            {
+                RegisterRepo = new RegistrationRepositary();
+            }
+            base.Initialize(requestContext);
+        } 
+
+        //
+        // GET: /Registration/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Registration/Create
+
+        [HttpPost]
+        public ActionResult Create(Registration reg)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    RegisterRepo.Register(reg);
+                }
+                else
+                {
+                    return View();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ModelValidation/Models/RegistrationModels.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ModelValidation.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "Please enter name.")]
+        [StringLength(20, ErrorMessage = "Name must be 20 char max.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter phone number.")]
+        [StringLength(10, ErrorMessage = "Phone number must be 10 digit max.")]
+        [Display(Name = "Phone Number")]
+        public long PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter email.")]
+        [StringLength(20, ErrorMessage = "Email must be 20 char max.")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$", ErrorMessage = "Please enter valid email id.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter user name.")]
+        [StringLength(20, ErrorMessage = "User name must be 20 char max.")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter password.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password name must be in between 6 to 20 char.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and confirm pssword must match")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public Address AddressDetails { get; set; }
+    }
+
+    public class Address
+    {
+        [Required(ErrorMessage = "Please enter Street name.")]
+        [StringLength(20, ErrorMessage = "Streen name must be 20 char max.")]
+        [Display(Name = "Street Name")]
+        public string StreetName { get; set; }
+
+        [Required(ErrorMessage = "Please enter state.")]
+        [StringLength(20, ErrorMessage = "State must be 20 char max.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter country.")]
+        [StringLength(20, ErrorMessage = "Country must be 20 char max.")]
+        public string Country { get; set; }
+    }
+
+    public interface IRegistrationRepositary
+    {
+        void Register(Registration reg);
+    }
+
+    public class RegistrationRepositary : IRegistrationRepositary
+    {
+        public void Register(Registration reg)
+        {
+            // Save new Registration to DB.
+        }
+    }
+}
